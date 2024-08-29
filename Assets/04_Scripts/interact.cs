a928c0b83e3f06a6096ae10fa82293339f3e941f
foming0652 <51737537+foming0652@users.noreply.github.com>
2024-08-29 10:51:12 +0900

    V9
    버그남

--- Assets/04_Scripts/interact.cs
+++ Assets/04_Scripts/interact.cs
@@ -9,17 +9,17 @@ public class interact : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _intText;
     [SerializeField] private float _checkRate = 0.05f;
     [SerializeField] private float _maxDistance = 3.0f;
-    [SerializeField] private LayerMask _layerMask;
+    public LayerMask _layerMask;
 
     private float _lastCheckTime;
     private GameObject _curObj;
 
     private IInteractable _curInt;
-    private Camera _camera;
+    
 
     void Start()
     {
-        _camera = Camera.main;
+        
         _layerMask = LayerMask.GetMask("Interactable");
     }
 
@@ -29,10 +29,13 @@ public class interact : MonoBehaviour
         {
             _lastCheckTime = Time.time;
 
-            Ray ray = _camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
-            RaycastHit hit;
+           RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, _maxDistance, _layerMask);
+            Debug.DrawRay(transform.position, Vector2.right * _maxDistance, Color.red);
 
-            if (Physics.Raycast(ray, out hit, _maxDistance, _layerMask))
+
+
+
+            if (hit.collider != null)
             {
                 if (hit.collider.gameObject != _curObj)
                 {
@@ -47,16 +50,20 @@ public class interact : MonoBehaviour
                 _curInt = null;
                 _intText.gameObject.SetActive(false);
             }
+            Debug.Log(_curInt);
         }
         
         if(_curInt != null && Input.GetKeyDown(KeyCode.E))
         {
             _curInt.Interaction();
         }
+
+        
     }
 
     void SetPromptText()
     {
+        Debug.Log("맞음");
         _intText.gameObject.SetActive(true);
         _intText.text = _curInt.GetText();
 
